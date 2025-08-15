874849bc499c561b8ef857e22c7c3eea59624870
Unique Pun <uniquepun20@gmail.com>
2025-08-15 18:14:42 +0545

    Initial commit of MyBlog advanced project

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+# from django.contrib import admin
+# from .models import Post, Comment
+
+# admin.site.register(Post)
+# admin.site.register(Comment)
+
+
+from django.contrib import admin
+from .models import Post, Comment, Category
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('categories', 'created_at')
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(Category)
