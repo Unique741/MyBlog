461b5c874e7950eb9e3a8bbde5aee31af59f1ed3
Unique Pun <uniquepun20@gmail.com>
2025-08-24 14:41:37 +0545

    Use environment variables for DEBUG and ALLOWED_HOSTS

--- myblog/settings.py
+++ myblog/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Split by commas, e.g. "myblog-x-1.onrender.com,www.myblog.com"
-ALLOWED_HOSTS = os.getenv("myblog-x-001.onrender.com", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
-SECRET_KEY = os.getenv("xxxMatrix", "unsafe-secret-key")
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
 
 
